9fa0bff255550afb5afc94a52cf291d83823a583
marcelo2335 <marcelo23@gmail.com>
2014-05-14 01:16:42 -0600

    nuevos archivos

--- js/_vti_cnf/jquery.jplayer.js
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|02 Oct 2012 14:15:10 -0000
-vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|indexb.html index.html
-vti_author:SR|Maria-PC\\Maria
-vti_modifiedby:SR|Maria-PC\\Maria
-vti_timecreated:TR|02 Oct 2012 14:15:10 -0000
-vti_cacheddtm:TX|02 Oct 2012 14:15:10 -0000
-vti_filesize:IR|84134
